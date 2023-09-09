f02fd2e6766aae04b62620e92a34795d02e5ef35
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-09-09 19:51:54 +0430

    add authentication to the app

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 // const Joi = require("joi");
+const config = require("config");
 const genreRouter = require("./routes/genres");
 const customerRouter = require("./routes/customers");
 const movie = require("./routes/movies")
 const rental = require("./routes/rentals")
 const users = require("./routes/users")
 const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+
+// console.log(config.get("jwtPrivateKey"));
+// console.log(config.has("jwtPrivateKey"));
+// if(!config.has("jwtPrivateKey")) {
+if(!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined")
+  process.exit(1)
+}
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/vidly")
@@ -19,6 +29,7 @@ app.use("/api/customer", customerRouter)
 app.use("/api/movie", movie)
 app.use("/api/rental", rental)
 app.use("/api/users", users)
+app.use("/api/auth", auth)
 
 const port = process.env.PORT || 3000;
 
---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const config = require("config");
+const router = express.Router();
+const _ = require("lodash");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("userName or password is invalid");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res.status(400).send("userName or password is invalid");
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  
+  res.send(token);
+});
+
+function validate(req) {
+  const schema = Joi.object({
+    email: Joi.string().required().min(5).max(255).email(),
+    password: Joi.string().required().min(5).max(255),
+  });
+
+  return schema.validate(req);
+}
+
+module.exports = router;
