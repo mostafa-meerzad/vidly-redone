79088ac186b35c509f3bcdb1ad3eda823abe4e1f
mostafa <mostafameerzad@gmail.com>
2024-02-05 21:31:44 +0430

    prepare the app for writing tests

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const logger = require("../utils/winstonLogger");
+const config = require("config");
 
 module.exports = function () {
+  const db = config.get("db");
+
   mongoose
-    .connect("mongodb://127.0.0.1:27017/vidly", { useUnifiedTopology: true })
-    .then(() => logger.info("connected to the database"))
+    .connect(db, { useUnifiedTopology: true })
+    .then(() => logger.info(`connected to the database... ${config.get("db")}`))
     .catch((e) => logger.error("something failed during connecting to db ", e));
 };
